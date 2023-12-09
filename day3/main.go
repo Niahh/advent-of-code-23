25a3e5a906846faca0b8b33f193dcece6da5c1c8
Niahh <samy.ouastani@gmail.com>
2023-12-09 13:19:08 +0100

    - day 3 part 2 and some small refactoring

--- day3/main.go
+++ day3/main.go
@@ -23,19 +23,28 @@ func readInput(testFile string) []string {
 	return strings.Split(string(data), "\n")
 }
 
-func parseInput(testFile string) []Indexes {
+func parseInput(testFile string, part int) []Indexes {
 	var parsedInput []Indexes
 
 	var patternNumbers string = "[0-9]+"
 	numbersRegex := regexp.MustCompile(patternNumbers)
 
-	var patternSymbols string = "[^\\d\\.]"
+	var patternSymbols string
+
+	if part == 1 {
+		// Get everything that is not a digit or a dot
+		patternSymbols = "[^\\d\\.]"
+	} else if part == 2 {
+		// Need to spot only the stars
+		patternSymbols = "[\\*]"
+	} else {
+		panic("Please enter the correct number of the part you want to solve")
+	}
 	symbolRegex := regexp.MustCompile(patternSymbols)
 
 	for _, line := range readInput(testFile) {
 		// Find the index of the numbers
 		numbersMatches := numbersRegex.FindAllStringSubmatchIndex(line, -1)
-		fmt.Println(numbersMatches)
 		// find the indexes of the symbols
 		symbolMatches := symbolRegex.FindAllStringSubmatchIndex(line, -1)
 		parsedInput = append(parsedInput, Indexes{numbersMatches, symbolMatches})
@@ -56,25 +65,9 @@ func getNumbersFromInput(testFile string) [][][]string {
 	return parsedNumbers
 }
 
-func getSymbolsLitterals(testFile string) map[string]int {
-	var patternSymbols string = "[=%@&\\*\\#\\+\\$\\-\\/]"
-	symbolRegex := regexp.MustCompile(patternSymbols)
-
-	var parsedSymbol = make(map[string]int)
-
-	for _, line := range readInput(testFile) {
-		symbolsMatches := symbolRegex.FindAllStringSubmatch(line, -1)
-		if len(symbolsMatches) > 0 {
-			parsedSymbol[symbolsMatches[0][0]] += 1
-		}
-	}
-
-	return parsedSymbol
-}
-
 func part1(testFile string) int {
 
-	data := parseInput(testFile)
+	data := parseInput(testFile, 1)
 	sum := 0
 
 	numbers := getNumbersFromInput(testFile)
@@ -92,12 +85,11 @@ func part1(testFile string) int {
 						}
 						sum += tmp
 						alreadyPicked = true
-						break
 					}
 				}
 			}
 			// check bottom row
-			if index < len(parseInput(testFile))-1 && !alreadyPicked {
+			if index < len(data)-1 && !alreadyPicked {
 				for _, symbIndexData := range data[index+1].Symbols {
 					if symbIndexData[0] >= positions[0]-1 && symbIndexData[1] <= positions[1]+1 {
 						tmp, err := strconv.Atoi(numbers[index][indNum][0])
@@ -106,7 +98,6 @@ func part1(testFile string) int {
 						}
 						sum += tmp
 						alreadyPicked = true
-						break
 					}
 				}
 			}
@@ -146,17 +137,84 @@ func part1(testFile string) int {
 	return sum
 }
 
+func part2(testFile string) int {
+	data := parseInput(testFile, 2)
+	sum := 0
+
+	numbers := getNumbersFromInput(testFile)
+
+	for index, row := range data {
+		for _, positions := range row.Symbols {
+
+			var numbersToMultiply []int
+
+			// check top row
+			if index > 0 && len(data[index-1].Numbers) > 0 {
+				for loopedIndex, numbIndexData := range data[index-1].Numbers {
+					tmpString := numbers[index-1][loopedIndex][0]
+					tmp, err := strconv.Atoi(tmpString)
+					if err != nil {
+						panic(err)
+					}
+					if numbIndexData[0] >= (positions[0]-(len(tmpString))) && numbIndexData[0] <= positions[0]+1 {
+
+						numbersToMultiply = append(numbersToMultiply, tmp)
+					}
+				}
+			}
+			// check bottom row
+			if index < len(data)-1 {
+				for loopedIndex, numbIndexData := range data[index+1].Numbers {
+					tmpString := numbers[index+1][loopedIndex][0]
+					tmp, err := strconv.Atoi(numbers[index+1][loopedIndex][0])
+					if err != nil {
+						panic(err)
+					}
+					if numbIndexData[0] >= (positions[0]-(len(tmpString))) && numbIndexData[0] <= positions[0]+1 {
+
+						numbersToMultiply = append(numbersToMultiply, tmp)
+					}
+				}
+			}
+
+			// check left
+			if positions[0] > 0 {
+				for loopedIndex, numbIndexData := range row.Numbers {
+					if numbIndexData[1] == positions[0] {
+						tmp, err := strconv.Atoi(numbers[index][loopedIndex][0])
+						if err != nil {
+							panic(err)
+						}
+						numbersToMultiply = append(numbersToMultiply, tmp)
+						break
+					}
+				}
+			}
+
+			// check right
+
+			for loopedIndex, numbIndexData := range row.Numbers {
+				if numbIndexData[0] == positions[1] {
+					tmp, err := strconv.Atoi(numbers[index][loopedIndex][0])
+					if err != nil {
+						panic(err)
+					}
+					numbersToMultiply = append(numbersToMultiply, tmp)
+					break
+				}
+			}
+
+			if len(numbersToMultiply) == 2 {
+				sum += numbersToMultiply[0] * numbersToMultiply[1]
+			}
+		}
+
+	}
+	return sum
+}
+
 func main() {
 	testFile := "input.txt"
-	// testFileTestBelow := "tests/below.txt"
-	// testFileTestUp := "tests/up.txt"
-	// testFileTestLeft := "tests/left.txt"
-	// testFileTestRight := "tests/right.txt"
-	// testFileTestSymbols := "tests/symbols.txt"
 	fmt.Println(part1(testFile))
-	// fmt.Println(part1(testFileTestBelow))   // must be equals to 666
-	// fmt.Println(part1(testFileTestUp))      // must be equals to 555
-	// fmt.Println(part1(testFileTestLeft))    // must be equals to 111
-	// fmt.Println(part1(testFileTestRight)) // must be equals to 111
-	// fmt.Println(part1(testFileTestSymbols)) // must be equals to 1110
+	fmt.Println(part2(testFile))
 }
